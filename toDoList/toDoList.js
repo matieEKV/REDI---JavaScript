e3e74ae9272796e77597049614c2ff12d6da97e6
MKanunnikau <matea.ekv@gmail.com>
2025-04-26 17:45:54 +0200

    removed a clear function

--- toDoList/toDoList.js
+++ toDoList/toDoList.js
@@ -2,12 +2,9 @@ let btn = document.getElementById('add-btn');
 let list = document.getElementById('task-list');
 let input = document.getElementById('task-input');
 
-function clearInput () {
-    input.value = '';
-}
 btn.addEventListener('click', function () {
     const newItem = document.createElement('li');
     newItem.innerText = input.value;
     list.appendChild(newItem);
-    clearInput();
+    input.value = '';
 })
